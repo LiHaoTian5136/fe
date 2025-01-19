2b3d9c2516ca270d29e1eedb372918de5f6cb4e2
LiHaoTian5136 <lihaotian5136@gmail.com>
2025-01-19 12:53:34 +0100

    Update fe.lua

--- fe.lua
+++ fe.lua
@@ -1,8 +1,8 @@
--- Validate the global 'key' variable
+-- Make sure 'key' is globally accessible
 local expectedKey = "aZwhUYZHywZBZYs"
 if _G.key ~= expectedKey then
     error("Incorrect key. Terminating script.")
 end
 
--- Rest of the script continues here if key is correct
+-- Rest of the script continues here if the key is correct
 print("Key validated. Running the rest of the script...")
